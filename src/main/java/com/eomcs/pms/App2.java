b7fbeb8e0337c2c20e4b283b447eb0cc3d59af66
Gunmok-Lee <Gunmok.L@gmail.com>
2020-07-17 15:08:04 +0900

    project1 clear

---
+++ src/main/java/com/eomcs/pms/App2.java
@@ -0,0 +1,13 @@
+package com.eomcs.pms;
+
+public class App2 {
+    public static void main(String[] args) {
+        System.out.print("[프로젝트]\n");
+        System.out.print("번호: 1201\n");
+        System.out.print("내용: 소규모 팀을 위한 프로젝트 관리 시스템을 개발한다.\n");
+        System.out.print("시작일: 2020-01-01\n");
+        System.out.print("종료일: 2020-12-31\n");
+        System.out.print("만든이: 홍길동\n");
+        System.out.print("팀원: 홍길동,김구,유관순,안중근,윤봉길\n");
+    }
+}
